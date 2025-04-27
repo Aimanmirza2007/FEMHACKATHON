7e0e4592a90f69f3e02bece7dec5d43856b1fd05
Aimanmirza2007 <sm501623@gmail.com>
2025-04-27 16:02:52 +0500

    first commit

--- website.js
+++ website.js
@@ -39,3 +39,37 @@ function moveCard(event) {
     button.innerText = "Completed";
   }
 }
+
+
+  // Import the functions you need from the SDKs you need
+  import { initializeApp } from "https://www.gstatic.com/firebasejs/11.6.1/firebase-app.js";
+  import { getDatabase,set,ref } from "https://www.gstatic.com/firebasejs/11.6.1/firebase-database.js";
+  // TODO: Add SDKs for Firebase products that you want to use
+  // https://firebase.google.com/docs/web/setup#available-libraries
+
+  // Your web app's Firebase configuration
+  // For Firebase JS SDK v7.20.0 and later, measurementId is optional
+  const firebaseConfig = {
+    apiKey: "AIzaSyDuYyAH9SKPIdONIWpoAvjXpoPCZYGqKeM",
+    authDomain: "femhack-database.firebaseapp.com",
+    projectId: "femhack-database",
+    storageBucket: "femhack-database.firebasestorage.app",
+    messagingSenderId: "119187500485",
+    appId: "1:119187500485:web:a992a6ea6cb9e06b03efac",
+    measurementId: "G-8B0LCX5EQX"
+  };
+
+  // Initialize Firebase
+  const app = initializeApp(firebaseConfig);
+ const dp = getDatabase(app)
+ document.getElementById("addButton").addEventListener('click',function(event){
+  event.preventDefault()
+  set(ref(db,"userdata/"+ document.getElementById("title").value),
+{
+  title: document.getElementById("title").value,
+  discription: document.getElementById("description").value,
+  category: document.getElementById("category").value,
+})
+alert("data save successfully")
+ })
+
